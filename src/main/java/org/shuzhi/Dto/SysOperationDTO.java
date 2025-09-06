301dae6508e4325dbf37b78b5285c0a7359eca7b
shuzhi <1428767439@163.com>
2025-09-06 17:49:04 +0800

    improve #000 优化操作记录的时间

--- src/main/java/org/shuzhi/Dto/SysOperationDTO.java
+++ src/main/java/org/shuzhi/Dto/SysOperationDTO.java
@@ -1,10 +1,13 @@
 package org.shuzhi.Dto;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * <p>
@@ -38,5 +41,6 @@ public class SysOperationDTO {
     /**
      * 创建时间
      */
-    private String createDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createDate;
 }
